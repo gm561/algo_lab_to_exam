7fc3621d945b4950accd3153817ae5e6371065e0
gregory561 <noemail@noemail.com>
2012-12-23 14:35:34 +0100

    demolition found bug in sets.

--- week1/demolition2.cpp
+++ week1/demolition2.cpp
@@ -15,6 +15,7 @@
 using namespace std;
 
 typedef int64_t num_t;
+
 struct Floor {
     num_t weight;
     num_t capacity;
@@ -25,7 +26,7 @@ struct Floor {
 	weight(w),
 	capacity(cap),
 	cost(co),
-	weight_under(weight_under_){}
+	weight_under(weight_under_) { }
 
     bool operator<(const Floor& floor) const {
 	return weight_under + capacity < floor.weight_under + floor.capacity;
@@ -33,7 +34,6 @@ struct Floor {
 };
 
 int main(int argc, char *argv[]) {
-
     cin.sync_with_stdio(false);
     cout.sync_with_stdio(false);
 
@@ -52,32 +52,28 @@ int main(int argc, char *argv[]) {
 	num_t current_cost = floors[0].cost;
 	num_t total_weight = floors[0].weight;
 
-	set<Floor> not_blew;
-	not_blew.insert(floors[0]);
+	vector<Floor> not_blew;
+	not_blew.push_back(floors[0]);
 
 	for(int i = 1; i < n; ++i) {
-	    bool stop = false;
-
             floors[i].weight_under = total_weight;
 	    total_weight += floors[i].weight;
 
-	    set<Floor>::iterator it = not_blew.begin();
-	    while(!not_blew.empty() && it != not_blew.end()) {
-//		set<Floor>::const_iterator cit = not_blew.begin();
+	    for(vector<Floor>::iterator it = not_blew.begin();
+		it != not_blew.end(); ) {
 		Floor floor = *it;
-		if(floor.capacity + floor.weight_under - total_weight >= 0) {
-//		    stop = true;
-		    ++it;
-		}
-		else {
-		    current_cost -= it->cost;
+		if( floor.capacity + floor.weight_under - total_weight < 0 ) {
 		    not_blew.erase(it);
 		    it = not_blew.begin();
+		    current_cost -= floor.cost;
+		}
+		else {
+		    ++it;
 		}
 	    }
 
 	    current_cost += floors[i].cost;
-	    not_blew.insert(floors[i]);
+	    not_blew.push_back(floors[i]);
 
 	    min_cost = (min_cost > current_cost) ? current_cost : min_cost;
 	}
